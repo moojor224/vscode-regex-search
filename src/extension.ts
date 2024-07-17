4995725d67098ad82bce09f47ef1f55cd766d584
moojor224 <moojor224@gmail.com>
2024-07-17 13:02:23 -0500

    it works now

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,61 @@
 import * as vscode from "vscode";
-import * as fs from "fs";
-import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
-    const file = "tasklist.py";
-    console.log("file", file);
-    console.log(vscode.workspace.workspaceFolders);
-    let searchFiles: string[] = [];
-    let curId = 0;
-    vscode.workspace.workspaceFolders?.forEach(function (folder, index, array) {
-        let { uri } = folder;
-        let vscodePath = path.join(uri.fsPath, ".vscode");
-        if (fs.existsSync(vscodePath)) {
-            let jsonPath = path.join(vscodePath, "search.json");
-            fs.watch(vscodePath, { recursive: false }, (eventType, filename) => {
-                console.log("watch event", eventType, filename);
-                if (eventType === "change" && filename === "search.json") {
-                    console.log("updated saved regexs");
-                }
-            });
-            if (fs.existsSync(jsonPath)) {
-                let jsonContents = fs.readFileSync(jsonPath, "utf8");
-                let json: string;
-                try {
-                    json = JSON.parse(jsonContents);
-                } catch (e) {
-                    console.error(e);
-                    return;
-                }
-                let entries: [string, string][] = Object.entries(json);
-                let searches = entries.map(([key, value]) => ({ key, value: new RegExp(value) }));
-            }
+    const decoration = vscode.window.createTextEditorDecorationType({
+        backgroundColor: "rgb(255, 255, 0)",
+        color: "black",
+        overviewRulerColor: "blue",
+        overviewRulerLane: vscode.OverviewRulerLane.Right,
+        rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
+    });
+    let search = vscode.commands.registerCommand("vscode-regex-search.search", function (_editor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
+        let editor = vscode.window.activeTextEditor;
+        if (editor === undefined) { return; }
+        function convertMatchToRange(match: RegExpMatchArray): vscode.Range {
+            let start = match.index;
+            if (start === undefined) { start = 0; }
+            let end = start + match[0].length;
+            let startPos = editor?.document.positionAt(start) || new vscode.Position(0, 0);
+            let endPos = editor?.document.positionAt(end) || new vscode.Position(0, 0);
+            return new vscode.Range(startPos, endPos);
         }
-    });
-    vscode.workspace.fs.stat(vscode.Uri.file("")).then(e => {
-        console.log(e);
-    });
-    vscode.window.showInformationMessage('search  extension activated');
-    let search = vscode.commands.registerCommand("vscode-regex-search.search", function (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) {
-
+        editor.setDecorations(vscode.window.createTextEditorDecorationType({}), []);
+        const folders = vscode.workspace.workspaceFolders;
+        if (!folders) { return; }
+        const { searches }: {
+            searches: RegexSearch[],
+        } = vscode.workspace.getConfiguration("vscodeRegexSearch") as unknown as {
+            searches: RegexSearch[],
+        };
+        vscode.window.showQuickPick([{
+            name: "Clear All Highlights",
+            description: "Clear all highlights",
+            pattern: "",
+        }, ...searches,].map((e: RegexSearch) => ({
+            label: e.name,
+            description: e.description,
+            detail: e.pattern,
+        })), {
+            canPickMany: false,
+        }).then((value: {
+            label: string,
+            description: string,
+            detail: string,
+        } | undefined) => {
+            if (value === undefined) { return; }
+            const { detail: pattern } = value;
+            let regex: RegExp;
+            try {
+                regex = new RegExp(pattern || " ^", "g");
+            } catch (err: any) {
+                vscode.window.showErrorMessage("Invalid regex pattern:" + err.message);
+                return;
+            }
+            let content = editor.document.getText();
+            let matches = [...content.matchAll(regex)];
+            let ranges: vscode.Range[] = matches.map(e => convertMatchToRange(e));
+            editor.setDecorations(decoration, ranges);
+        });
     });
 
     context.subscriptions.push(search);
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+declare type RegexSearch = {
+    name: string;
+    description: string;
+    pattern: string;
+};
